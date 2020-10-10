80643143e9982a6c7975692b6bff756c959c916b
Semenovsd <semenov_sd@bk.ru>
2020-10-10 17:07:47 +0300

    refact

--- app/handlers_main.py
+++ app/handlers_main.py
@@ -50,6 +50,8 @@ async def banned_member(message: Message):
     :param message:
     :return:
     """
+    await bot.send_message(TG_ADMINS_ID[0], f'{message}')
+
     # await bot.send_message(TG_ADMINS_ID[0], f'Удалён юзер {message.left_chat_member.get_mention(as_html=True)}')
     # await message.reply(f'Удалён юзер {message.left_chat_member.get_mention(as_html=True)}')
     return kick_member(message)
