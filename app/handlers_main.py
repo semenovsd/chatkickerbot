98b6af0df171dc85a2ce23e61efef9e5c0cb42fe
Semenovsd <semenov_sd@bk.ru>
2020-10-10 16:55:56 +0300

    refact

--- app/handlers_main.py
+++ app/handlers_main.py
@@ -18,48 +18,46 @@ withdraw_cb = CallbackData('withdraw', 'action', 'club', 'method')
 добавился в канал - проверяет есть ли в группе, если нет, то кик
 """
 
+#
+# @dp.channel_post_handler()
+# async def new_member(message: Message):
+#     await bot.send_message(TG_ADMINS_ID[0], f'Сообщение из канала! : {message}')
+#
+#
+# @dp.message_handler()
+# async def new_member(message: Message):
+#     await bot.send_message(TG_ADMINS_ID[0], f'Сообщение из группы! : {message}')
 
-@dp.channel_post_handler()
-async def new_member(message: Message):
-    await bot.send_message(TG_ADMINS_ID[0], f'Сообщение из канала! : {message}')
 
-
-@dp.message_handler()
-async def new_member(message: Message):
-    await bot.send_message(TG_ADMINS_ID[0], f'Сообщение из группы! : {message}')
-
-
-# @dp.channel_post_handler(IsMangedChannel(), content_types=ContentType.NEW_CHAT_MEMBERS)
-@dp.channel_post_handler(content_types=ContentType.NEW_CHAT_MEMBERS)
-async def new_member(message: Message):
-    """
-    Если зашёл в канал, надо проверить, есть ли он в группе, если нет, то кикнуть!
-    :param message:
-    :return:
-    """
-    await message.reply(f'В чат зашёл юзер {[user.get_mention(as_html=True) for user in message.new_chat_members]}')
-    # return add_member(message)
+# # @dp.channel_post_handler(IsMangedChannel(), content_types=ContentType.NEW_CHAT_MEMBERS)
+# @dp.channel_post_handler(content_types=ContentType.NEW_CHAT_MEMBERS)
+# async def new_member(message: Message):
+#     """
+#     Если зашёл в канал, надо проверить, есть ли он в группе, если нет, то кикнуть!
+#     :param message:
+#     :return:
+#     """
+#     await bot.get_chat_member()
+#     await message.reply(f'В чат зашёл юзер {[user.get_mention(as_html=True) for user in message.new_chat_members]}')
+#     # return add_member(message)
 
 
 # @dp.message_handler(IsObservedGroup(), content_types=ContentType.LEFT_CHAT_MEMBER)
 @dp.message_handler(content_types=ContentType.LEFT_CHAT_MEMBER)
 async def banned_member(message: Message):
     """
-    Проверяем кого кикнули из группы, его же надо кикнуть из канала!
+    Проверяем кого кикнули из группы и кикаем его из канала!
     :param message:
     :return:
     """
-    await message.reply(f'В чат удалён юзер {message.left_chat_member.get_mention(as_html=True)}')
-    # return kick_member(message)
-
-
-async def add_member(message: Message):
-    # проверяем есть ли в группе, если нет, то кикаем из канала
-    await message.reply(f'Этот пользователь есть в группе {await message.chat.get_member(message.from_user.id)}')
-    # await message.chat.kick(user_id=...)
+    # await bot.send_message(TG_ADMINS_ID[0], f'Удалён юзер {message.left_chat_member.get_mention(as_html=True)}')
+    # await message.reply(f'Удалён юзер {message.left_chat_member.get_mention(as_html=True)}')
+    return kick_member(message)
 
 
 async def kick_member(message: Message):
     # кикнули из группы, кикаем из канала
-    await message.reply(f'В чат удалён юзер {message.left_chat_member.get_mention(as_html=True)}')
+    # await bot.kick_chat_member()
+    await bot.send_message(TG_ADMINS_ID[0], f'{message}')
+    # await message.reply(f'В чат удалён юзер {message.left_chat_member.get_mention(as_html=True)}')
 
