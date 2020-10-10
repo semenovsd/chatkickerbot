1cb9a8677b354310eb38ea61955d230df0428322
Semenovsd <semenov_sd@bk.ru>
2020-10-10 14:14:12 +0300

    refact

---
+++ app/filters.py
@@ -0,0 +1,16 @@
+from aiogram.dispatcher.filters import BoundFilter
+from aiogram.types import Message
+
+from config import TG_NOTICE_GROUP_ID, TG_NOTICE_CHANNEL_ID
+
+
+class IsObservedGroup(BoundFilter):
+    async def check(self, message: Message) -> bool:
+        return message.chat.id == TG_NOTICE_GROUP_ID
+
+
+class IsMangedChannel(BoundFilter):
+    async def check(self, message: Message) -> bool:
+        return message.chat.id == TG_NOTICE_CHANNEL_ID
+
+
--- app/handlers_main.py
+++ app/handlers_main.py
@@ -1,26 +1,49 @@
-from typing import Dict, Union
 
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters import CommandStart
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, Message, ContentType
+from aiogram.types import Message, ContentType
 from aiogram.utils.callback_data import CallbackData
-from aiogram.utils.exceptions import MessageNotModified
 
-from database import User
-from load_all import bot, dp
+from filters import IsMangedChannel, IsObservedGroup
+from load_all import dp
 
 club_cb = CallbackData('deposit', 'club')
 withdraw_cb = CallbackData('withdraw', 'action', 'club', 'method')
 
 
-@dp.callback_query_handler(text='start')
-@dp.message_handler(CommandStart())
-async def start_cmd_handler(message: Union[Message, CallbackQuery], state: FSMContext, user: User):
-    """
-    Стартовый экран приветствия.
-    :param message:
-    :param state:
-    :param user:
-    :return:
-    """
-    pass
+# goal - observe users in group and kick users in channel
+
+"""
+добавили в группу ничего не делаем.
+удалили из группы - удаляем из канала
+добавился в канал - проверяет есть ли в группе, если нет, то кик
+"""
+
+
+# сделать два фильтра на группу и канал
+@dp.message_handler()
+async def new_member(message: Message):
+    await message.reply(f'Сообщение из чата: {message=}')
+
+
+# @dp.message_handler(IsMangedChannel(), content_types=ContentType.NEW_CHAT_MEMBERS)
+@dp.message_handler(content_types=ContentType.NEW_CHAT_MEMBERS)
+async def new_member(message: Message):
+    await message.reply(f'В чат зашёл юзер {[user.get_mention(as_html=True) for user in message.new_chat_members]}')
+    # return add_member(message)
+
+
+# @dp.message_handler(IsObservedGroup(), content_types=ContentType.LEFT_CHAT_MEMBER)
+@dp.message_handler(content_types=ContentType.LEFT_CHAT_MEMBER)
+async def banned_member(message: Message):
+    await message.reply(f'В чат удалён юзер {message.left_chat_member.get_mention(as_html=True)}')
+    # return kick_member(message)
+
+
+async def add_member(message: Message):
+    # проверяем есть ли в группе, если нет, то кикаем из канала
+    await message.reply(f'Этот пользователь есть в группе {await message.chat.get_member(message.from_user.id)}')
+
+
+async def kick_member(message: Message):
+    # кикнули из группы, кикаем из канала
+    await message.reply(f'В чат удалён юзер {message.left_chat_member.get_mention(as_html=True)}')
+
