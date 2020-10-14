bfe75e867e12c633ecc0fa1f01a7dfba5ba51e86
Semenovsd <semenov_sd@bk.ru>
2020-10-14 11:47:45 +0300

    refact

--- app/handlers_main.py
+++ app/handlers_main.py
@@ -15,17 +15,17 @@ from load_all import dp, bot
 #     :param message:
 #     :return:
 #     """
-#     await bot.send_message(TG_ADMINS_ID[0], f'{message.chat.id}')
+#     await bot.send_message(TG_ADMINS_ID[0], f'Сообщение из канала: {message.chat.id}')
 #
 #
-# @dp.message_handler()
-# async def new_member(message: Message):
-#     """
-#     Данный хэндлер ловит любое сообщение из группы из отправляет админу сообщение с его айди.
-#     :param message:
-#     :return:
-#     """
-#     await bot.send_message(TG_ADMINS_ID[0], f'{message.chat.id}')
+@dp.message_handler()
+async def new_member(message: Message):
+    """
+    Данный хэндлер ловит любое сообщение из группы из отправляет админу сообщение с его айди.
+    :param message:
+    :return:
+    """
+    await bot.send_message(TG_ADMINS_ID[0], f'Сообщение из группы: {message.chat.id}')
 
 
 @dp.message_handler(IsObservedGroup(), content_types=ContentType.LEFT_CHAT_MEMBER)
